c4e3bfe10bdf126aa7e521d547251e8877910661
YE-EUN <musiclove529@naver.com>
2024-03-25 17:17:13 +0900

    feat: AbeekRepository 생성
    related to: #7

---
+++ cms/src/main/java/jbnu/it/cms/repository/AbeekRepository.java
@@ -0,0 +1,7 @@
+package jbnu.it.cms.repository;
+
+import jbnu.it.cms.domain.entity.Abeek;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AbeekRepository extends JpaRepository<Abeek, String> {
+}
