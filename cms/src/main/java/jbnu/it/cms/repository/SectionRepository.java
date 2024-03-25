b6227fb382f3c6a91ca93bb88efc77fdab8bf407
YE-EUN <musiclove529@naver.com>
2024-03-25 17:10:14 +0900

    test: SectionRepositoryTest 변경

--- cms/src/main/java/jbnu/it/cms/repository/SectionRepository.java
+++ cms/src/main/java/jbnu/it/cms/repository/SectionRepository.java
@@ -8,5 +8,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface SectionRepository extends JpaRepository<Section, SectionPK> {
-    List<Section> findByCourse(Course course);
 }
